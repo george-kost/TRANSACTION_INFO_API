7d88f4aa58184e3816e9c06b5095ef5b9913c003
george-kost <67106410+george-kost@users.noreply.github.com>
2024-06-15 14:34:51 +0300

    Add files via upload

---
+++ TransactionInfoApi/src/main/java/com/kost/TransactionInfoApi/TransactionInfoApiApplication.java
@@ -0,0 +1,13 @@
+package com.kost.TransactionInfoApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionInfoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionInfoApiApplication.class, args);
+	}
+
+}
---
+++ TransactionInfoApi/src/test/java/com/kost/TransactionInfoApi/TransactionInfoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kost.TransactionInfoApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionInfoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
